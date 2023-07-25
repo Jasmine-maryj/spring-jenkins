b28475e607126d1fe3477e4ace4c98f23b6268b5
Jasmine Mary <jasminemary630@gmail.com>
2023-07-25 22:36:51 +0530

    First commit

---
+++ src/main/java/com/dev/SpringJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.slf4j.*;
+import javax.annotation.PostConstruct;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
