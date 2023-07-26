276bddebe330fee43838fb8ebd70c62c5734b59a
Jasmine Mary <jasminemary630@gmail.com>
2023-07-26 15:12:44 +0530

    sixth commit

--- src/main/java/com/dev/SpringJenkinsApplication.java
+++ src/main/java/com/dev/SpringJenkinsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJenkinsApplication {
 	
-	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	public static final Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 	
 	@PostConstruct
 	public void init() {
